377869fd4528edff974a04b10846c31fa0d9aa45
Rna341 <rna.tariq.aboouf@gmail.com>
2024-03-05 23:26:09 +0200

    Betty styled

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,31 +3,34 @@
 /**
 * bubble sort - Function that sorts an array of ints
 * using bubble sort algorithm
-* 
+*
 * @array: array of ints
-* 
+*
 * @size: size of array
 */
 
-void bubble_sort(int *array, size_t size) {
-    int tmp;
-    size_t i, j;
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j;
 
-    if (!array || !size)
-        return;
-    
-    i = 0;
-    while (i < size)
-    {
-        for (j = 0; j < size - 1; j++) {
-            if (array[j] > array[j+1]) {
-                tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
-                print_array(array, size);
+	if (!array || !size)
+		return;
 
-            }
-        }
-        i++;
-    }
+	i = 0;
+
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				tmp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
