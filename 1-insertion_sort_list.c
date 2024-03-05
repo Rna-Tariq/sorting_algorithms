dd3e198d071470b11c12d533192a52bd5b46dafd
Rna341 <rna.tariq.aboouf@gmail.com>
2024-03-06 01:04:18 +0200

    Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,53 @@
 #include "sort.h"
 
 /**
-* swap - swaps 2 nodes in a doubly-linked list
-*
-* @a: address of first node
-* @b: address of second node
-*/
-
-void swap(listint_t *a, listint_t *b)
+ * swap - Swap nodes on list.
+ * @list: pointer to head of list.
+ * @current: pointer to current node of list.
+ * @previous: pointer to previous node of list.
+ */
+void swap(listint_t **list, listint_t *current, listint_t *previous)
 {
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = a;
-	b->next = a;
+	if (current->next)
+		(current->next)->prev = previous;
+
+	current->prev = previous->prev;
+	current->next = previous;
+
+	if (previous->prev)
+		(previous->prev)->next = current;
+	else
+		(*list) = current;
 }
 
 /**
-* insertion_sort_list - insertion sorts a doubly-linked list
-*
-* @list: address of pointer to head node
-*/
-
+ * insertion_sort_list - Sort a list with insertion sort algorithm.
+ * @list: list of integers
+ * Return: nothing
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t i, j;
+	listint_t *previous = NULL, *current = NULL, *next = NULL;
 
-	if (!list || !(*list) || !((*list)->next))
+	if (!list || !(*list))
 		return;
-	i = (*list)->next;
 
-	while (i)
+	current = (*list)->next;
+	while (current)
 	{
-		j = i;
-		i = i->next;
-		while (j && j->prev)
+		next = current->next;
+		previous = current->prev;
+
+		while (previous && current->n < previous->n)
 		{
-			if (j->prev->n > j->n)
-			{
-				swap(j->prev, j);
-				if (!j->prev)
-					*list = j;
-				print_list((const listint_t *)*list);
-			}
-			else
-				j = j->prev;
+			previous->next = current->next;
+
+			swap(list, current, previous);
+			previous->prev = current;
+			previous = current->prev;
+
+			print_list(*list);
 		}
+		current = next;
 	}
 }
